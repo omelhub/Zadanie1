f0f0ba30eaaaa7b4234bbdbca6f127193c432b75
Zloy_Zliden <justdanilka@gmail.com>
2022-03-03 12:26:36 +0700

    Добавьте файлы проекта.

---
+++ Cylinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Cylinder
+    {
+        public double D { set; get; }
+        public double H { set; get; }
+        public double V { set; get; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder();
+            Console.Write("Введите диаметр цилиндра, м: ");
+            cylinder.D = Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Введите высоту цилиндра, м: ");
+            cylinder.H = Convert.ToUInt32(Console.ReadLine());
+            cylinder.V = Math.PI / 4 * cylinder.D * cylinder.D * cylinder.H;
+            Console.WriteLine($"Объем цилиндрического резервуара: {cylinder.V} м^3.");
+            Console.ReadKey();
+        }
+    }
+}
